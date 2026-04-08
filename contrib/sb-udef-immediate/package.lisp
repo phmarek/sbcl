5e594745ae98bd5c41c6310d2abf0e5ec3c905bf
Philipp Marek <philipp@marek.priv.at>
2026-04-08 12:05:35 +0200

    SB-UDEF-IMMEDIATE contrib with tests.

---
+++ contrib/sb-udef-immediate/package.lisp
@@ -0,0 +1,32 @@
+
+(defpackage :sb-udef-immediate
+  (:documentation "User-defined integer types")
+  (:use :common-lisp)
+  (:import-from :sb-vm
+                #:udef-inttype-widetag)
+  (:import-from :sb-int
+                #:udef-inttype-type-of
+                #:udef-inttype-p
+                #:udef-inttype-tag
+                #:udef-inttype
+                #:+udef-tag-bits+
+                )
+  (:export #:def-udef-immediate
+           #:udef-inttype-type-of
+           #:udef-inttype-p
+           #:udef-general-get-value
+           #:def-column-struct
+           #:column-struct-resize
+           #:make-wrapped-udef-accessor
+           #:column-struct-reset
+           #:column-struct-clear
+           #:column-struct-size
+           #:column-struct-last-index
+           #:column-struct-get-struct
+
+           #:map-c-s-range
+           #:c-s-values
+           #:with-c-s-slots
+         ))
+
+(require :sb-introspect)
